9b1c973366ce6cadf986d4669c84e39d472ba3fc
vinothini2463 <vinothini2463@gmail.com>
2026-07-10 15:56:11 +0530

    Update CorsConfig.java

--- demo086/src/main/java/com/example/demo086/security/CorsConfig.java
+++ demo086/src/main/java/com/example/demo086/security/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5500",
-                "http://127.0.0.1:5500"
+                "http://127.0.0.1:5500",
+            "https://vinothini2463.github.io"
         ));
 
         config.setAllowedMethods(List.of(
