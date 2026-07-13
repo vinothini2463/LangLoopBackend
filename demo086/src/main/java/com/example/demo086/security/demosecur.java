4da99230c085bb26d9ffe0d77f4dbc796d230514
vinothini2463 <vinothini2463@gmail.com>
2026-07-13 13:26:07 +0530

    Update demosecur.java

--- demo086/src/main/java/com/example/demo086/security/demosecur.java
+++ demo086/src/main/java/com/example/demo086/security/demosecur.java
@@ -8,18 +8,12 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import java.beans.Customizer;
-import java.util.List;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.demo086.services.CustomUserDetailsService;
 import com.example.demo086.util.JwtFilter;
@@ -56,24 +50,35 @@ public class demosecur {
         return provider;
     }
 
-    
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
+
+        http
                 .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**","/api/decks/**","/api/flashcards/**","/api/metrics/**"
-                        ,"/api/sessions/**","/api/users/**").permitAll()
-                        .requestMatchers("/api/users/**").hasRole("ADMIN")
-                        .anyRequest().authenticated())
+
+                        .requestMatchers(
+                                "/auth/**",
+                                "/api/decks/**",
+                                "/api/flashcards/**",
+                                "/api/metrics/**",
+                                "/api/sessions/**"
+                        ).permitAll()
+
+                        .requestMatchers("/api/users/**")
+                        .hasRole("ADMIN")
+
+                        .anyRequest()
+                        .authenticated())
+
                 .authenticationProvider(authenticationProvider())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .formLogin(form -> form.disable())
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
